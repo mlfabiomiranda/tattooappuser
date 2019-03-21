b3486b6ee9ba32dc0e80ac935f8c3e7a5306b078
Fabio Miranda <fabio.miranda@mercadolivre.com>
2019-03-20 23:58:32 -0300

    first commit

---
+++ src/main/kotlin/com/newidea/user/TattooappuserApplication.kt
@@ -0,0 +1,11 @@
+package com.newidea.user
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TattooappuserApplication
+
+fun main(args: Array<String>) {
+	runApplication<TattooappuserApplication>(*args)
+}
